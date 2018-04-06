2952c2c66bed7f55fd6310607455133db40d4d7d
Alan Russell <ajrussellaudio@gmail.com>
2018-04-06 13:15:23 +0100

    add Record acceptance criteria

---
+++ specs/Record.test.js
@@ -0,0 +1,9 @@
+const Record = require("../models/Record");
+const assert = require("assert");
+
+describe("Record", () => {
+  it("should have an artist");
+  it("should have a title");
+  it("should have a genre");
+  it("should have a price");
+});

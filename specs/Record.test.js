fec44f3cfc12c0daed050f507c6b8942c1d4edd7
Alan Russell <ajrussellaudio@gmail.com>
2018-04-06 13:27:23 +0100

    add title to record

--- specs/Record.test.js
+++ specs/Record.test.js
@@ -2,12 +2,20 @@ const Record = require("../models/Record");
 const assert = require("assert");
 
 describe("Record", () => {
+  let record;
+
+  beforeEach(() => {
+    record = new Record("Amon Tobin", "Permutation", "electronic", 10);
+  });
+
   it("should have an artist", () => {
-    const record = new Record("Amon Tobin", "Permutation", "electronic", 10);
     assert.equal("Amon Tobin", record.artist);
   });
 
-  it("should have a title");
+  it("should have a title", () => {
+    assert.equal("Permutation", record.title);
+  });
+
   it("should have a genre");
   it("should have a price");
 });

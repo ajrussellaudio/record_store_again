27840f597068e9aa1c9e3bcb761b5c1c966d1997
Alan Russell <ajrussellaudio@gmail.com>
2018-04-06 13:22:31 +0100

    add artist to record

--- specs/Record.test.js
+++ specs/Record.test.js
@@ -2,7 +2,11 @@ const Record = require("../models/Record");
 const assert = require("assert");
 
 describe("Record", () => {
-  it("should have an artist");
+  it("should have an artist", () => {
+    const record = new Record("Amon Tobin", "Permutation", "electronic", 10);
+    assert.equal("Amon Tobin", record.artist);
+  });
+
   it("should have a title");
   it("should have a genre");
   it("should have a price");

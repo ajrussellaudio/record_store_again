5c4adca7343532cb744689f6b38ddcf98937bd8e
Alan Russell <ajrussellaudio@gmail.com>
2018-04-06 21:19:20 +0100

    add record collector with buy and sell criteria

---
+++ specs/RecordCollector.test.js
@@ -0,0 +1,7 @@
+const RecordCollector = require("../models/RecordCollector");
+const assert = require("assert");
+
+describe("RecordCollector", () => {
+  it("should buy records");
+  it("should sell records");
+});

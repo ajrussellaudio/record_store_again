182132ac919382dc401c8e33b3f1ec0a47bee2a4
Alan Russell <ajrussellaudio@gmail.com>
2018-04-06 13:45:26 +0100

    add record store acceptance criteria

---
+++ specs/RecordStore.test.js
@@ -0,0 +1,8 @@
+const RecordStore = require("../models/RecordStore");
+const assert = require("assert");
+
+describe("RecordStore", () => {
+  it("should have a name");
+  it("should have a city");
+  it("should have an inventory");
+});
